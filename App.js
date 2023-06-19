0ad3d6d64a42e2f2ab7894d4da35bf63ea835157
Sanyuktha Kumbla <sanyu@Sanyukthas-MacBook-Pro.local>
2023-06-18 22:27:29 -0400

    episode-01

---
+++ App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", {}, "I'm an h1 tag"),React.createElement("h1", {}, "I'm an h1 tag")]
+  )
+);
+
+const heading = React.createElement("h1",{id:"heading"},"Hello from React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
